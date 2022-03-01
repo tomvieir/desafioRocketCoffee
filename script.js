4edd6387a9006c00db8fdb08a5b39745b7c719ed
tomvieir <wevertonbvieira@gmail.com>
2022-03-01 19:05:47 -0300

    Refazendo CSS em Mobile-first

---
+++ script.js
@@ -0,0 +1,15 @@
+document.querySelectorAll("#Close-menu").forEach(function(element) {
+    element.addEventListener("click", () =>{
+        document.querySelector(".container").classList.toggle("show-menu")
+    })
+})
+
+
+
+
+
+// function openMenu() {
+//     document.querySelector(".hamburguer").classList.toggle = ("hamburger-close")
+// }
+
+
